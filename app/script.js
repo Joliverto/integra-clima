89f03e7a4f222653cc86fe840458fca5903c9d06
Joao Neto <joaonetow9@gmail.com>
2025-07-28 22:17:09 -0300

    Adição das seções restantes

--- app/script.js
+++ app/script.js
@@ -1,4 +1,52 @@
+// Menu Hamburguer
 const hamburguer = document.querySelector('#burguer');
 const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+// Galeria de Fotos
+
+const modal = document.getElementById("modal")
+const modalImg = document.getElementById("modal-img")
+const itemsGaleria = document.querySelectorAll(".item-galeria")
+const closeBtn = document.getElementsByClassName("close")[0];
+const prevBtn = document.querySelector(".prev")
+const nextBtn = document.querySelector(".next")
+
+let currentImageIndex
+
+const openModal = (index) => {
+    modal.style.display = "block"
+    modalImg.src = itemsGaleria[index].querySelector("img").src
+    currentImageIndex = index
+}
+
+itemsGaleria.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        openModal(index)
+    })
+});
+
+const closeModal = () => {
+    modal.style.display = "none"
+}
+closeBtn.onclick = closeModal
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal()
+    }
+}
+
+const showPreviousImage = () => {
+    currentImageIndex = (currentImageIndex - 1 + itemsGaleria.length) % itemsGaleria.length
+    modalImg.src = itemsGaleria[currentImageIndex].querySelector("img").src
+}
+
+const showNextImage = () => {
+    currentImageIndex = (currentImageIndex + 1) % itemsGaleria.length
+    modalImg.src = itemsGaleria[currentImageIndex].querySelector("img").src
+}
+
+prevBtn.addEventListener("click", showPreviousImage)
+nextBtn.addEventListener("click", showNextImage)
